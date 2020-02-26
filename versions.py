6b1e372aa24eb34d1517a45706e800a0caa7314d
supplee <supplee@no-reply.github.com>
2020-02-25 19:34:28 -0800

    Created versions.py to display library versions for compatibility testing

---
+++ versions.py
@@ -0,0 +1,20 @@
+# versions.py
+
+# scipy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+#matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+#pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+#sklearn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
+#statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
